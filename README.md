e7d809ca6054028100939ae0288f46f95c8391ba
Danilo <dznery@gmail.com>
2016-10-17 01:41:20 -0200

    Optimized...?

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ constraint satisfaction heuristics.
 
 [![Floobits Status](https://floobits.com/dnery/futoshiki-solver.svg)](https://floobits.com/dnery/futoshiki-solver/redirect)
 
-# The game rules
-  From Wikipedia
+# What is Futoshiki?
+From Wikipedia
 
-Futoshiki (不等式 futōshiki?), or More or Less, is a logic puzzle game from
-Japan. Its name means "inequality". It is also spelled hutosiki (using
-Kunrei-shiki romanization).
+| Futoshiki (不等式 futōshiki?), or More or Less, is a logic puzzle game from
+| Japan. Its name means "inequality". It is also spelled hutosiki (using
+| Kunrei-shiki romanization).
 
-# Perf-eval status
-- cpython: 19:35m
-- pypy3: 1:50m
+Example of a 5x5 Futoshiki puzzle...
+
+![pre-sol](https://upload.wikimedia.org/wikipedia/commons/0/00/Futoshiki1.png)
+
+...And it's solution
+
+![pos-sol](https://upload.wikimedia.org/wikipedia/commons/3/37/Futoshiki2.png)
+
+# Perf-eval Status
+  -> cpython: 19:35m
+  -> pypy (pypy2): 1:28m
--- solve-a-shiki.py
+++ solve-a-shiki.py
@@ -1,4 +1,6 @@
-import sys
+from __future__ import print_function
+from sys import argv
+from sys import stdin
 
 
 class Pos:
@@ -14,6 +16,7 @@ class Test:
     board = []
     values = []
     constraints = []
+    branches = 0
 
     def __init__(self, bdim, board, values, constraints):
         self.bdim = bdim
@@ -22,133 +25,120 @@ class Test:
         self.constraints = constraints
 
 
-def empty_pos(board):
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0:
+def empty_pos(test):
+    for i in range(test.bdim):
+        for j in range(test.bdim):
+            if test.board[i][j] == 0:
                 return Pos(i, j)
-
     return None
 
 
-def empty_pos_lrv(board, values):
-    max = 10  # greater than possible
+def lrv_empty_pos(test):
+    max = 10
     pos = None
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0 and (len(values[i][j]) > 0 and
-                                     len(values[i][j]) < max):
-                max = len(values[i][j])
+    for i in range(test.bdim):
+        for j in range(test.bdim):  # nesse teste, fazer check se values[i] > 0
+            if test.board[i][j] == 0 and (len(test.values[i][j]) > 0 and
+                                          len(test.values[i][j]) < max):
+                max = len(test.values[i][j])  # max = 2^(bdim)
                 pos = Pos(i, j)
     return pos
 
 
-def valid_pos(board, pos, num, constraints):
-    # check cols
-    for i in range(len(board)):
-        if board[i][pos.y] == num:
+def valid_pos(test, pos, num):
+    # check rows, cols
+    for i in range(test.bdim):
+        if test.board[i][pos.y] == num:
             return False
-    # check rows
-    for j in range(len(board)):
-        if board[pos.x][j] == num:
+        if test.board[pos.x][i] == num:
             return False
     # check constraints
-    for c in constraints[pos.x][pos.y]:
-        if board[c[1].x][c[1].y] == 0:
+    for c in test.constraints[pos.x][pos.y]:
+        if test.board[c[1].x][c[1].y] == 0:
             continue
-
-        if c[0] is True:
-            if board[c[1].x][c[1].y] > num:
-                return False
-        else:
-            if board[c[1].x][c[1].y] < num:
-                return False
-
+        elif c[0] is True and test.board[c[1].x][c[1].y] > num:
+            return False
+        elif c[0] is False and test.board[c[1].x][c[1].y] < num:
+            return False
     return True
 
 
-def remove_value(values, pos, num, constraints):
+def propagate_removal(test, pos, num):
+    test.board[pos.x][pos.y] = num
+    # clear row, col
     memory = []
-    for i in range(len(values)):
-        if num in values[i][pos.y]:
-            values[i][pos.y].remove(num)
+    for i in range(test.bdim):
+        if num in test.values[i][pos.y]:
+            test.values[i][pos.y].remove(num)
             memory.append((num, Pos(i, pos.y)))
-        if num in values[pos.x][i]:
-            values[pos.x][i].remove(num)
+        if num in test.values[pos.x][i]:
+            test.values[pos.x][i].remove(num)
             memory.append((num, Pos(pos.x, i)))
-
-    for c in constraints[pos.x][pos.y]:
-        if c[0] is True:
-            removals = [x for x in values[c[1].x][c[1].y] if x > num]
-        else:
-            removals = [x for x in values[c[1].x][c[1].y] if x < num]
-
-        for removal in removals:
-            values[c[1].x][c[1].y].remove(removal)
-            memory.append((removal, Pos(c[1].x, c[1].y)))
-
+    # satisfy constraints
+    for c in test.constraints[pos.x][pos.y]:
+        for x in list(test.values[c[1].x][c[1].y]):  # has to be a copy
+            if (c[0] is True and x > num) or (c[0] is False and x < num):
+                test.values[c[1].x][c[1].y].remove(x)
+                memory.append((x, Pos(c[1].x, c[1].y)))
     return memory
 
 
-def rewind_value(values, memory):
-    for pos in memory:
-        values[pos[1].x][pos[1].y].append(pos[0])
+def rewind_removal(test, pos, memory):
+    test.board[pos.x][pos.y] = 0
+    for entry in memory:
+        test.values[entry[1].x][entry[1].y].append(entry[0])
 
 
-def no_more_values(board, values):
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0 and len(values[i][j]) == 0:
-                return True
+def no_more_values(test, pos):
+    for i in range(test.bdim):
+        if test.board[pos.x][i] == 0 and len(test.values[pos.x][i]) == 0:
+            return True
+        if test.board[i][pos.y] == 0 and len(test.values[i][pos.y]) == 0:
+            return True
     return False
 
 
-def solve_shiki(board, constraints, branches):
-    pos = empty_pos(board)
+def solve_shiki(test):
+    pos = empty_pos(test)
     # success
     if pos is None:
         return True
     # failure by effort
-    if branches[0] > 1000000:
+    if test.branches > 1000000:
         return False
+    # all possible values overall
+    for num in range(1, test.bdim + 1):
+        if valid_pos(test, pos, num):
+            test.board[pos.x][pos.y] = num
+            test.branches += 1
 
-    for num in range(1, len(board) + 1):  # all numbers 1 to dim + 1
-        if valid_pos(board, pos, num, constraints):
-            branches[0] += 1
-            board[pos.x][pos.y] = num
-
-            if solve_shiki(board, constraints, branches):
+            if solve_shiki(test):
                 return True
-
-            board[pos.x][pos.y] = 0
+            test.board[pos.x][pos.y] = 0
     # failure by exhaustion
     return False
 
 
-def solve_shiki_fwd(board, values, constraints, branches):
-    # pos = empty_pos(board)
-    pos = empty_pos_lrv(board, values)
+def fwd_solve_shiki(test):
+    pos = lrv_empty_pos(test)
     # success
     if pos is None:
         return True
     # failure by effort
-    if branches[0] > 1000000:
+    if test.branches > 1000000:
         return False
-
-    static_vals = list(values[pos.x][pos.y])
-    for num in static_vals:
-        branches[0] += 1
-        board[pos.x][pos.y] = num
-        # remove value from row, col
-        mem = remove_value(values, pos, num, constraints)
-        # forward check pass
-        if not no_more_values(board, values):
-            if solve_shiki_fwd(board, values, constraints, branches):
+    # all possible values for this position
+    for num in list(test.values[pos.x][pos.y]):
+        # remove value from board
+        mem = propagate_removal(test, pos, num)
+        test.branches += 1
+        # do forward check pass
+        if not no_more_values(test, pos):
+            if fwd_solve_shiki(test):
                 return True
 
-        # reinsert value in row, col
-        rewind_value(values, mem)
-        board[pos.x][pos.y] = 0
+        # reinsert value in board
+        rewind_removal(test, pos, mem)
     # failure by exhaustion
     return False
 
@@ -158,71 +148,80 @@ def print_shiki(board):
         print(row)
 
 
+def non_empty_lines(stream):
+    lines = stream.readlines()
+    for line in list(lines):
+        if line.strip() == '':
+            lines.remove(line)
+    return iter(lines)
+
+
 def read_experiment(stream):
-    next(stream)  # skip test count
+    lines = non_empty_lines(stream)
+    next(lines)  # skip test count
 
     tests = []
-    for line in stream:
-        # ignore empty lines
-        if not line.strip():
-            continue
-        # read dims
+    for line in lines:
+        # read dimensions
         line = line.split()
         bdim, cdim = int(line[0]), int(line[1])
-        # read board
+
+        # read game board
         board = []
         for i in range(bdim):
-            line = next(stream).split()
+            line = next(lines).split()
             board.append([int(x) for x in line])
-        # read constraints
+
+        # generate constraint array
         constraints = []
         for i in range(bdim):
             constraints.append([])
             for j in range(bdim):
                 constraints[i].append([])
+
+        # populate constraint array
         for i in range(cdim):
-            line = next(stream).split()
+            line = next(lines).split()
             bign = Pos(int(line[2]) - 1, int(line[3]) - 1)
             smln = Pos(int(line[0]) - 1, int(line[1]) - 1)
             constraints[bign.x][bign.y].append((True, smln))
             constraints[smln.x][smln.y].append((False, bign))
-        # generate values
+
+        # generate possible values
         values = []
         for i in range(bdim):
             values.append([])
             for j in range(bdim):
                 values[i].append([x for x in range(1, bdim + 1)])
+
+        # clear pre-occupied positions
+        test = Test(bdim, board, values, constraints)
         for i in range(bdim):
             for j in range(bdim):
                 if board[i][j] != 0:
-                    remove_value(values, Pos(i, j), board[i][j], constraints)
+                    propagate_removal(test, Pos(i, j), board[i][j])
         # append
-        tests.append(Test(bdim, board, values, constraints))
-
+        tests.append(test)
     return tests
 
 
 def main():
-    if len(sys.argv) > 1:
-        stream = open(sys.argv[1], 'r')
+    if len(argv) > 1:
+        stream = open(argv[1], 'r')
     else:
-        stream = sys.stdin
+        stream = stdin
     tests = read_experiment(stream)
 
-    tno = 0
-    for test in tests:
-        tno += 1
-        branches = [0]
-        # status = solve_shiki(test.board, test.constraints, branches)
-        status = solve_shiki_fwd(test.board, test.values, test.constraints,
-                                 branches)
-        print(tno, ':')
+    for i in range(len(tests)):
+        # status = solve_shiki(test)
+        status = fwd_solve_shiki(tests[i])
 
         if status:
-            print_shiki(test.board)
-            print('done in', branches[0], 'branches')
+            print(i + 1, ':')
+            print_shiki(tests[i].board)
+            print('done in', tests[i].branches, 'branches')
         else:
-            print('no solution:', branches[0], 'branches taken')
+            print(i + 1, ': no solution :', tests[i].branches, 'branches taken')
 
 if __name__ == '__main__':
     main()
